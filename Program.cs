a091a3cc604e34e98021827174425977fd8943b8
cglrclh <caglarculha35@gmail.com>
2022-09-01 02:02:49 +0300

    .vscodde

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        
+        if(sayi % 2 != 0)
+            Console.WriteLine("Weird");
+        else if(2<=sayi && sayi<=5 && sayi % 2 == 0)
+            Console.WriteLine("Not Weird"); 
+        else if(6<=sayi && sayi<=20)
+            Console.WriteLine("Weird");
+        else
+            Console.WriteLine("Not Weird");
+
+    }
+}
